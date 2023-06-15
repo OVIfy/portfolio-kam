d4e1e1eb4934cb24afc8520a95e8faf5414a0ac8
DESKTOP-13RQSSJ\HP <Ezechukwuchirimmadu@gmail.com>
2023-06-15 06:17:05 +0100

    slider

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,18 +1,27 @@
 const slides = [
     {
-        "fill" : "bg-red-500"
+        "fill" : "bg-red-500",
+        "src" : "/Images/slides/1.jpg"
     },
     {
-        "fill" : "bg-blue-200"
+        "fill" : "bg-blue-200",
+        "src" : "/Images/slides/2.jpg"
     },
     {
-        "fill" : "bg-pink-200"
+        "fill" : "bg-pink-200",
+        "src" : "/Images/slides/3.jpg"
     },
     {
-        "fill" : "bg-yellow-400"
+        "fill" : "bg-yellow-400",
+        "src" : "/Images/slides/4.jpg"
     },
     {
-        "fill" : "bg-purple-200"
+        "fill" : "bg-purple-200",
+        "src" : "/Images/slides/5.jpg"
+    },
+    {
+        "fill" : "bg-purple-200",
+        "src" : "/Images/slides/6.jpg"
     },
 ]
 
